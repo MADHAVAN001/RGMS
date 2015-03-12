f9fcaaa515a35978097d7be824562f5e72fd32c1
MADHAVAN001 <MADHAVAN001@e.ntu.edu.sg>
2015-03-12 16:14:22 +0800

    update from madhavan

--- src/com/software_engineering/rgms/Surgery.java
+++ src/com/software_engineering/rgms/Surgery.java
@@ -1,5 +1,22 @@
 package com.software_engineering.rgms;
 
 public class Surgery {
-
+	String name;
+	String comments;
+	public void setname(String name)
+	{
+		this.name = name;
+	}
+	public void setcomments(String comments)
+	{
+		this.comments = comments;
+	}
+	public String getcomments()
+	{
+		return comments;
+	}
+	public String getname()
+	{
+		return name;
+	}
 }
