501ef3b337243e7d346e4fc1b5ea2e69e6960e20
mohitgambani <mohitgambani4@gmail.com>
2015-03-12 21:31:06 +0800

    Update Doctor.java

--- src/com/software_engineering/rgms/Doctor.java
+++ src/com/software_engineering/rgms/Doctor.java
@@ -1,10 +1,21 @@
 package com.software_engineering.rgms;
 
+import java.util.GregorianCalendar;
+
 public class Doctor extends HumanActor{
+	
 	String specialization;
-	String location;
-	public Doctor(){
-		
+	String practicelocation;
+	int holidaysleft;
+	
+	public Doctor(String username, String name, String emailid,
+			GregorianCalendar dob, String address, String phoneno,
+			GregorianCalendar registerdate, String specialization,
+			String practicelocation, int holidaysleft){
+		super(username, name, emailid, dob, address, phoneno, registerdate);
+		this.specialization = specialization;
+		this.practicelocation = practicelocation;
+		this.holidaysleft = holidaysleft;
 	}
 	public String getSpecialization(){
 		return specialization;
@@ -12,10 +23,16 @@ public class Doctor extends HumanActor{
 	public void setSpecialization(String specialization){
 		this.specialization = specialization;
 	}
-	public String getLocation(){
-		return location;
+	public String getPracticeLocation(){
+		return practicelocation;
 	}
-	public void setLocation(String location){
-		this.location = location;
+	public void setPracticeLocation(String location){
+		this.practicelocation = location;
+	}
+	public int getHolidaysLeft(){
+		return holidaysleft;
+	}
+	public void setHolidaysLeft(int holidaysleft){
+		this.holidaysleft = holidaysleft;
 	}
 }
