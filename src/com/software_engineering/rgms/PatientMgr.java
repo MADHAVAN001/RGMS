a2eec799d3bafbf9a937005d21059e1e220aa020
mohitgambani <mohitgambani4@gmail.com>
2015-03-12 21:30:05 +0800

    Update PatientMgr.java

--- src/com/software_engineering/rgms/PatientMgr.java
+++ src/com/software_engineering/rgms/PatientMgr.java
@@ -1,5 +1,19 @@
 package com.software_engineering.rgms;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
 
 public class PatientMgr {
 	
+	protected HashMap <String, Patient>patients;
+	
+	public void createPatientAccount(String username, String name, String emailid,
+			GregorianCalendar dob, String address, String phoneno,
+			GregorianCalendar registerdate, int notifmethod){
+		patients.put(username, new Patient(username, name, emailid,
+				dob, address, phoneno, registerdate, notifmethod));
+	}
+	
+	public void deletePatientAccount(String username){
+		patients.remove(username);
+	}
 }
