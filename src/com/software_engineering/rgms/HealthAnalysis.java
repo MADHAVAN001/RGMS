a9ec91489dd0994ed772c5faf385c00d64d24791
MADHAVAN001 <MADHAVAN001@e.ntu.edu.sg>
2015-03-12 17:04:54 +0800

    Entitiy classes for my part

--- src/com/software_engineering/rgms/HealthAnalysis.java
+++ src/com/software_engineering/rgms/HealthAnalysis.java
@@ -1,5 +1,74 @@
 package com.software_engineering.rgms;
 
-public class HealthAnalysis {
+import java.util.GregorianCalendar;
 
+public class HealthAnalysis {
+	String diagnosis;
+	GregorianCalendar datetime;
+	float bill;
+	String suggestion;
+	int healthAnalysisid;
+	boolean billpaid;
+	String title;
+	String prescribefollowup;
+
+	public void setdiagnosis(String diagnosis) {
+		this.diagnosis = diagnosis;
+	}
+
+	public void setbill(float bill) {
+		this.bill = bill;
+	}
+
+	public void setsuggestion(String suggestion) {
+		this.suggestion = suggestion;
+	}
+
+	public void sethealthanalysisid(int healthanalysisid) {
+		this.healthAnalysisid = healthanalysisid;
+	}
+
+	public void setbillpaid(boolean billpaid) {
+		this.billpaid = billpaid;
+	}
+
+	public void settitle(String title) {
+		this.title = title;
+	}
+
+	public void setprescribefollowup(String prescribefollowup) {
+		this.prescribefollowup = prescribefollowup;
+	}
+
+	public String getdiagnosis() {
+		return this.diagnosis;
+	}
+
+	public GregorianCalendar getdatetime() {
+		return this.datetime;
+	}
+
+	public float getbill() {
+		return this.bill;
+	}
+
+	public String getsuggestion() {
+		return this.suggestion;
+	}
+
+	public int gethealthanalysisid() {
+		return this.healthAnalysisid;
+	}
+
+	public boolean getbillpaid() {
+		return this.billpaid;
+	}
+
+	public String gettitle() {
+		return this.title;
+	}
+
+	public String getprescribefollowup() {
+		return this.prescribefollowup;
+	}
 }
--- src/com/software_engineering/rgms/Medicines.java
+++ src/com/software_engineering/rgms/Medicines.java
@@ -1,5 +1,40 @@
 package com.software_engineering.rgms;
 
 public class Medicines {
-
+	int[] quantity;
+	String name;
+	String description;
+	int[] intakecon;
+	public void setquantity(int quantity[])
+	{
+		this.quantity = quantity;
+	}
+	public void setname(String name)
+	{
+		this.name = name;
+	}
+	public void setdescription(String description)
+	{
+		this.description = description;
+	}
+	public void set(int intakecon[])
+	{
+		this.intakecon = intakecon;
+	}
+	public String getname()
+	{
+		return name;
+	}
+	public int[] getquatity()
+	{
+	return quantity;	
+	}
+	public int[] getintakecon()
+	{
+		return intakecon;
+	}
+	public String getdescription()
+	{
+		return description;
+	}
 }
