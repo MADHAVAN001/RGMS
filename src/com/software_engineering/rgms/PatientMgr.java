f93e334f92ed15686f2697ba8ad262027ba63b69
mohitgambani <mohitgambani4@gmail.com>
2015-03-13 14:55:41 +0800

    Update PatientMgr.java

--- src/com/software_engineering/rgms/PatientMgr.java
+++ src/com/software_engineering/rgms/PatientMgr.java
@@ -1,10 +1,21 @@
 package com.software_engineering.rgms;
 import java.util.GregorianCalendar;
-import java.util.HashMap;
+import java.util.*;
 
 public class PatientMgr {
 	
 	protected HashMap <String, Patient>patients;
+	
+	public PatientMgr(){
+		DatabaseMgr db = new DatabaseMgr();
+		patients = new HashMap<String, Patient>();
+		patients = db.getData(1, patients);
+		Iterator<String> keySetIterator = patients.keySet().iterator();
+		while(keySetIterator.hasNext()){
+		  String key = keySetIterator.next();
+		  patients.get(key).apps = db.loadAppointments(key, patients.get(key).apps);
+		}
+	}
 	
 	public void createPatientAccount(String username, String name, String emailid,
 			GregorianCalendar dob, String address, String phoneno,
