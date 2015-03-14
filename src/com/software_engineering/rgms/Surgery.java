30cb6b89225b322bb293bfd4da25ebf1ad0573f8
mohitgambani <mohitgambani4@gmail.com>
2015-03-15 01:06:52 +0800

    Update Surgery.java

--- src/com/software_engineering/rgms/Surgery.java
+++ src/com/software_engineering/rgms/Surgery.java
@@ -3,6 +3,10 @@ package com.software_engineering.rgms;
 public class Surgery {
 	String name;
 	String comments;
+	Surgery(String name, String comments){
+		this.name = name;
+		this.comments = comments;
+	}
 	public void setname(String name)
 	{
 		this.name = name;
